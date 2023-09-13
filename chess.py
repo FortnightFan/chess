19708ff0454d14d83c153d60ed166de84a18f0ec
FortnightFan <ktran79904@gmail.com>
2023-09-13 15:42:15 -0700

    Fixed pawn and rook logic
    Rook and pawn movement and capture logic are completed. Need to be tested.

--- chess.py
+++ chess.py
@@ -28,7 +28,7 @@ def print_board(board):
 def add_piece(board, piece):
     board[piece.ypos][piece.xpos] = piece
 
-def piece_testbench(board,piece):
+def piece_testbench(board,piece):       #adds a given piece to the board at location. Board is printed, and possible captures and moves are printed out
     add_piece(board,piece)
     print_board(board)
     piece.find_poss_moves()
@@ -51,23 +51,28 @@ add_piece (board, PW2)
 PW3 = Pawn(0, 3, 6, board)
 add_piece (board, PW3)
 
-PW4 = Pawn(0, 4, 6, board)
-add_piece (board, PW4)
-
 PW5 = Pawn(0, 5, 6, board)
 add_piece (board, PW5)
 
 PW6 = Pawn(0, 6, 6, board)
 add_piece (board, PW6)
+
 PW7 = Pawn(0, 7, 6, board)
 add_piece (board, PW7)
+
+
+RW0 = Rook(0, 3, 4, board)
 
 """
 Black pawns (from top)
 """
-PB0 = Pawn(1, 1, 4, board)
-PB0.has_moved = True
-add_piece (board,PB0)
-print_board(board)
 
-piece_testbench(board,PB0)
+BP0 = Pawn (1,4,4,board)
+BP1 = Pawn (1,2,4,board)
+BP2 = Pawn (1,3,5,board)
+BP3 = Pawn (1,3,3,board)
+add_piece (board, BP1)
+add_piece (board, BP3)
+
+
+piece_testbench(board,RW0)
--- classes.py
+++ classes.py
@@ -50,7 +50,7 @@ class Pawn(Piece):
                 if self.board[y+2][x] == tile and self.board[y+1][x] == tile:
                     self.poss_moves.append((self.xpos,self.ypos+2))
                     self.poss_moves.append((self.xpos,self.ypos+1))
-            elif self.ypos < 8 and self.board[y][x] == tile:
+            elif self.ypos < 8 and self.board[y+1][x] == tile:
                 self.poss_moves.append((self.xpos,self.ypos+1))
                 
             if self.xpos == 0 and self.ypos < 8:                #find possible pawn captures, white
@@ -64,7 +64,9 @@ class Pawn(Piece):
                     self.poss_captures.append((x-1,y+1))
                 if self.board[y+1][x+1].color == 0:
                     self.poss_captures.append((x+1,y+1))
-                
+        self.poss_captures = list(set(self.poss_captures))    #removes duplicates
+        self.poss_moves = list(set(self.poss_moves))    #removes duplicates
+
     def clear_lists(self):
         self.poss_moves = []
         self.poss_captures = []
@@ -74,29 +76,82 @@ class Rook(Piece):
         return (f"R")
     
     def find_poss_moves(self):
-        x, y = self.xpos, self.ypos
-        while y < 8 and (self.board[y][x] == tile or self.board[y][x] == self):  # checks all squares south
-            self.poss_moves.append((x, y))
-            y += 1
+        if self.color == 0:     #white
+            x, y = self.xpos, self.ypos
+            while y < 8 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 1):  # checks all squares south
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                y += 1
 
-        y = self.ypos
-        while y >= 0 and (self.board[y][x] == tile or self.board[y][x] == self):  # checks all squares north
-            self.poss_moves.append((x, y))
-            y -= 1
+            y = self.ypos
+            while y >= 0 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 1):  # checks all squares north
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                y -= 1
 
-        y = self.ypos
-        x = self.xpos
-        while x < 8 and (self.board[y][x] == tile or self.board[y][x] == self):  # checks all squares east
-            self.poss_moves.append((x, y))
-            x += 1
+            y = self.ypos
+            x = self.xpos
+            while x < 8 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 1):  # checks all squares east
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                x += 1
 
-        x = self.xpos
-        while x >= 0 and (self.board[y][x] == tile or self.board[y][x] == self):  # checks all squares west
-            self.poss_moves.append((x, y))
-            x -= 1
+            x = self.xpos
+            while x >= 0 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 1):  # checks all squares west
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                x -= 1
             
-        self.poss_moves = list(set(self.poss_moves))    #removes duplicates
+            self.poss_moves = list(set(self.poss_moves))    #removes duplicates
+            self.poss_moves.remove((self.xpos,self.ypos))
+            self.poss_moves = [item for item in self.poss_moves if item not in self.poss_captures]
 
+            
+        if self.color == 1:     #black
+            x, y = self.xpos, self.ypos
+            while y < 8 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 0):  # checks all squares south
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                y += 1
+
+            y = self.ypos
+            while y >= 0 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 0):  # checks all squares north
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                y -= 1
+
+            y = self.ypos
+            x = self.xpos
+            while x < 8 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 0):  # checks all squares east
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                x += 1
+
+            x = self.xpos
+            while x >= 0 and (self.board[y][x] == tile or self.board[y][x] == self or self.board[y][x].color == 0):  # checks all squares west
+                self.poss_moves.append((x, y))
+                if self.board[y][x].color == 1:
+                    self.poss_captures.append((x,y))
+                    break
+                x -= 1
+            
+            self.poss_moves = list(set(self.poss_moves))    #removes duplicates
+            self.poss_moves.remove((self.xpos,self.ypos))
+            self.poss_moves = [item for item in self.poss_moves if item not in self.poss_captures]
 
 board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
