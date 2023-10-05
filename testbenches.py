9611ef898e67c37cbc031ac1ee4c1fb08c85b931
JakeyPooo <125299172+JakeyPooo@users.noreply.github.com>
2023-10-05 15:44:07 -0700

    ADDED ANOTHER TEST FOR WHITE ROOK

--- testbenches.py
+++ testbenches.py
@@ -18,7 +18,7 @@ Y    [0, 1, 2, 3, 4, 5, 6, 7],
 accessing a tile:   board[Y][X] => piece
 """
 
-def clear_board():
+def clear_board(): #call to clear board
     global board
     board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -41,8 +41,7 @@ def clear_board():
         for j in range (0,8):
             board[i][j] = tile
 
-
-def white_pin_testbench():        #expected output: Black rook can only move in 3 col, or capture the rook.    
+def white_rook_pin_testbench(): #expected output: Black rook can only move in 3 col, or capture the rook.    
     add_piece(board, King(1,3,0,board))  #adds piece to board with x,y coords
     add_piece(board, Rook(0,3,7,board))  #adds piece to board with x,y coords
     add_piece(board, Pawn(0,2,4,board))  #adds piece to board with x,y coords
@@ -58,7 +57,24 @@ def white_pin_testbench():        #expected output: Black rook can only move in 
     clear_all_lists()
     clear_board()
 
-def black_pin_testbench(): #expected output: White rook can only move in col 3, or capture white rook
+def white_rook_diff_initial_pin_testbench(): #expected output: Black rook can only move in col 6, or capture white rook.
+    add_piece(board, King(1,6,0,board))  #adds piece to board with x,y coords
+    add_piece(board, Rook(0,6,7,board))  #adds piece to board with x,y coords
+    add_piece(board, Pawn(0,5,4,board))  #adds piece to board with x,y coords
+
+    RB = Rook (1,6,4,board)
+    add_piece(board, RB)  #adds piece to board with x,y coords
+
+    find_all_poss_moves(board)
+
+    check_pin(board, RB) #removes possible moves that will put own king in check
+    piece_testbench(board, RB) #adds piece to board, prints board, all possible moves.
+
+    clear_all_lists()
+    clear_board()
+
+def black_rook_pin_testbench(): #expected output: White rook can only move in col 3, or capture white rook
+
     add_piece(board, King(0,3,0,board))
     add_piece(board, Rook(1,3,7,board))
     add_piece(board, Pawn(1,2,4,board))
@@ -72,11 +88,13 @@ def black_pin_testbench(): #expected output: White rook can only move in col 3, 
     piece_testbench(board, RW)
 
     clear_all_lists()
+    clear_board()
 
 print("BLACK ROOK TEST")
-white_pin_testbench()
-#print_board(board) #TEST TO MAKE SURE BOARD IS CLEARED
+white_rook_pin_testbench()
+print("\nBLACK ROOK DIFFERENT INITIAL LOCATION TEST")
+white_rook_diff_initial_pin_testbench()#INCORRECT OUTPUT. CAPTURES PAWN ONLY
 print("\nWHITE ROOK TEST")
-black_pin_testbench() #INCORRECT OUTPUT. CAPTURES PAWN ONLY
+black_rook_pin_testbench() #INCORRECT OUTPUT. CAPTURES PAWN ONLY
 
 
